e013ddea7381082047c43887dc4063d2b917a81a
guillametlucia <109171170+guillametlucia@users.noreply.github.com>
2023-06-08 13:32:53 +0100

    Add files via upload

---
+++ sbibm/tasks/repressilator/results/snpe/plot_sns.py
@@ -0,0 +1,38 @@
+#%%
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib.ticker import MaxNLocator
+
+def posterior_plot(data:str):
+    df = pd.read_csv(data)
+    df=df[df['Group']=='p'] 
+    plt.figure(figsize=(8, 8)) 
+    # Create PairGrid 
+    g = sns.PairGrid(df, vars=df.columns[:12], corner=True)
+    g = g.map_diag(sns.kdeplot)
+    g = g.map_offdiag(sns.kdeplot)
+    # Setting x and y axis limits
+    for ax in g.axes.flatten():
+        if ax is not None:
+            ax.set_xlim(-3, 3)
+            ax.set_ylim(-3, 3)
+            ax.xaxis.set_major_locator(MaxNLocator(nbins=3))
+            ax.yaxis.set_major_locator(MaxNLocator(nbins=3))
+    plt.suptitle('Pairplot of Posterior Samples', fontsize=50)
+    # # Create new legend
+    # legend_elements = [Patch(facecolor='blue', edgecolor='blue', label='p'),
+    #                    Patch(facecolor='orange', edgecolor='orange', label='r')]
+    # plt.legend(handles=legend_elements, title='Group', loc='upper right')
+    plt.show()
+
+posterior_plot('df_posterior_2_1000_smc_abc.csv')
+
+
+#from sbibm.visualisation import fig_posterior
+#fig = fig_posterior(task_name="repressilator", observation=1, samples=[posterior_samples])  
+#fig.show()
+#Note: Use fig.show() or fig.save() to show or save the figure
+# %%
